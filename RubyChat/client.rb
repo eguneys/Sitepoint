d52ad779e940d3f23cc5bad1328607ce1039281a
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2013-12-07 09:50:35 -0500

    creating base files for ruby chat

---
+++ RubyChat/client.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby -w
+require "socket"
+class Client
+  def initialize( server )
+
+  end
+end
---
+++ RubyChat/server.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby -w
+require "socket"
+class Server
+  def initialize( port, ip )
+
+  end
+end
