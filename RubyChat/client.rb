d388b86f7b89329a16018fdc15e6bb5b46c13fe9
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2013-12-07 10:02:47 -0500

    method implementation for handling request and responses between clients and server

--- RubyChat/client.rb
+++ RubyChat/client.rb
@@ -5,21 +5,32 @@ class Client
     @server = server
     @request = nil
     @response = nil
+    listen
+    send
   end
 
   def listen
     @response = Thread.new do
-      loop {
-
+      loop { # listen for ever
+        # listen the server responses
+        msg = @server.gets.chomp # gets the server messages
+        # show them in the console
+        puts "#{msg}"
       }
     end
   end
 
   def send
     @request = Thread.new do
-      loop {
-
+      loop {# write how much you want
+        # read from the console
+        msg = $stdin.gets.chomp # gets users input from command line
+        # when enter, send the message to the server
+        @server.puts( msg )
       }
     end
   end
 end
+
+server = TCPSocket.open( ip, port )
+client = Client.new( server )
--- RubyChat/server.rb
+++ RubyChat/server.rb
@@ -2,7 +2,7 @@
 require "socket"
 class Server
   def initialize( port, ip )
-    @server = nil
+    @server = TCPServer.open( ip, port )
     @connections = Hash.new
     @rooms = Hash.new
     @clients = Hash.new
@@ -13,7 +13,15 @@ class Server
 
   def run
     loop {
-      Thread.start do |client| # each client thread
+      Thread.start(@server.accept) do | client |
+        nick_name = client.gets.chomp.to_sym
+        @connections[:clients].each do |other_name, other_client|
+          if nick_name == other_name || client == other_client
+            client.puts "This username already exist"
+            Thread.kill self
+          end
+        end
+        @connections[:clients][nick_name] = client
       end
     }
   end
