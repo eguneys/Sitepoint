ae1a89d5607577837c1d308b226671b3cf7dd2a2
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2013-12-07 09:56:24 -0500

    methods for handling messages between clients and server

--- RubyChat/client.rb
+++ RubyChat/client.rb
@@ -2,6 +2,24 @@
 require "socket"
 class Client
   def initialize( server )
+    @server = server
+    @request = nil
+    @response = nil
+  end
 
+  def listen
+    @response = Thread.new do
+      loop {
+
+      }
+    end
+  end
+
+  def send
+    @request = Thread.new do
+      loop {
+
+      }
+    end
   end
 end
--- RubyChat/server.rb
+++ RubyChat/server.rb
@@ -2,6 +2,19 @@
 require "socket"
 class Server
   def initialize( port, ip )
+    @server = nil
+    @connections = Hash.new
+    @rooms = Hash.new
+    @clients = Hash.new
+    @connections[:server] = @server
+    @connections[:rooms] = @rooms
+    @connections[:clients] = @clients
+  end
 
+  def run
+    loop {
+      Thread.start do |client| # each client thread
+      end
+    }
   end
 end
